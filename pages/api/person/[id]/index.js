e35fb85797a8b4eef214eaffd9dbda4cda7cf97f
Naddiya <nadiamedkouri@gmail.com>
2021-04-28 16:41:37 +0200

    wip: :sob: put method

--- pages/api/person/[id]/index.js
+++ pages/api/person/[id]/index.js
@@ -1,12 +1,15 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
-export default async function getPerson(req, res) {
+export default async function getPersonById(req, res) {
     const db = await open({
         filename: './mydb.sqlite',
         driver: sqlite3.Database,
     });
-
-    const person = await db.get('select * from person where id=?', [req.query.id]);
+    if (req.method === 'PUT') {
+        const result = await statement.run(req.body.name, req.body.email, req.query.id);
+        const statement = await db.prepare('update person set name = ?, email= ? where id = ?');
+    };
+    const person = await db.get('select * from person where id = ?', [req.query.id]);
     res.json(person);
-};
+}
--- pages/api/person/[id]/vehicles.js
+++ pages/api/person/[id]/vehicles.js
@@ -2,11 +2,14 @@ import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
 export default async function getVehicleByPersonId(req, res) {
+    if (req.method !== 'GET') {
+        res.status(500).json({ message: 'Only GET requests allowd' })
+    }
     const db = await open({
         filename: './mydb.sqlite',
         driver: sqlite3.Database,
     });
-    
+
     const userVehicles = await db.all('select * from vehicle where ownerId=?', [req.query.id]);
 
     res.json(userVehicles);
